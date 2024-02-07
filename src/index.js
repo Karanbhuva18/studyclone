85c52f4c59d76def7ac2128b963f144a1da71167
unknown <Karanbhuva18>
2024-02-07 21:54:27 +0530

    studyclone code for mega project

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import rootReducer from "./reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { Toaster } from "react-hot-toast";
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
